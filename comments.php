d3eba8c54517095c112875cedca35c76350abb53
aareyad <aliakbar.reyad@gmail.com>
2022-10-25 11:33:19 +0600

    added comment

--- comments.php
+++ comments.php
@@ -17,28 +17,32 @@ if ( post_password_required() ) {
 <?php if ( have_comments() ): ?>
 	<?php
 	$comments_number = get_comments_number();
-	$comments_text   = $comments_number == 1 ? esc_html__( 'Comment', 'homlisti' ) : esc_html__( 'Comments', 'homlisti' );
+	$comments_text   = $comments_number == 1 ? esc_html__( 'Comment', 'cl-classified' ) : esc_html__( 'Comments', 'cl-classified' );
 	$comments_html   = number_format_i18n( $comments_number ) . ' ' . $comments_text;
 	$has_avatar      = get_option( 'show_avatars' );
 	$comment_class   = $has_avatar ? ' avatar-disabled' : '';
 	$comment_args    = [
-		'callback'    => 'RadiusTheme\Classified\Helper::comments_callback',
-		'reply_text'  => esc_html__( 'Reply', 'homlisti' ),
+		'callback'    => 'RadiusTheme\ClassifiedLite\Helper::comments_callback',
+		'reply_text'  => esc_html__( 'Reply', 'cl-classified' ),
 		'avatar_size' => 70,
 	];
 	?>
+    <div class="content-block-gap"></div>
+    <div class="site-content-block blog-comment">
+        <div class="main-title-block">
+            <h3 class="main-title"><?php echo esc_html( $comments_html ); ?></h3>
+        </div>
+        <div class="main-content">
+            <div class="comments-area">
+                <ul class="comment-list<?php echo esc_attr( $comment_class ); ?>">
+                    <?php wp_list_comments( $comment_args ); ?>
+                </ul>
+                <?php the_comments_navigation(); ?>
 
-    <div class="block-content mt-30 blog-comment">
-        <div class="widget-heading"><h3 class="heading-title"><?php echo esc_html( $comments_html ); ?></h3></div>
-        <div class="comment-box">
-            <ul class="comment-list<?php echo esc_attr( $comment_class ); ?>">
-				<?php wp_list_comments( $comment_args ); ?>
-            </ul>
-			<?php the_comments_navigation(); ?>
-
-			<?php if ( ! comments_open() ) : ?>
-                <div class="comments-closed"><?php esc_html_e( 'Comments are closed.', 'homlisti' ); ?></div>
-			<?php endif; ?>
+                <?php if ( ! comments_open() ) : ?>
+                    <div class="comments-closed"><?php esc_html_e( 'Comments are closed.', 'cl-classified' ); ?></div>
+                <?php endif; ?>
+            </div>
         </div>
     </div>
 <?php endif; ?>
@@ -56,17 +60,17 @@ $rdtheme_aria_req  = ( $rdtheme_req ? " required" : '' );
 $comment_form_fields = [
 	'author' =>
 		'<div class="row gutters-20"><div class="col-lg-6 form-group"><input type="text" id="author" name="author" value="' . esc_attr( $rdtheme_commenter['comment_author'] )
-		. '" placeholder="' . esc_attr__( 'Name', 'homlisti' ) . ( $rdtheme_req ? ' *' : '' ) . '" class="form-control"' . $rdtheme_aria_req . '></div>',
+		. '" placeholder="' . esc_attr__( 'Name', 'cl-classified' ) . ( $rdtheme_req ? ' *' : '' ) . '" class="form-control"' . $rdtheme_aria_req . '></div>',
 
 	'email' =>
 		'<div class="col-lg-6 form-group"><input id="email" name="email" type="email" value="' . esc_attr( $rdtheme_commenter['comment_author_email'] )
-		. '" class="form-control" placeholder="' . esc_attr__( 'Email', 'homlisti' ) . ( $rdtheme_req ? ' *' : '' ) . '"' . $rdtheme_aria_req . '></div></div>',
+		. '" class="form-control" placeholder="' . esc_attr__( 'Email', 'cl-classified' ) . ( $rdtheme_req ? ' *' : '' ) . '"' . $rdtheme_aria_req . '></div></div>',
 ];
 
 $comment_form_args = [
-	'class_submit'  => 'submit-btn',
+	'class_submit'  => 'submit btn-send',
 	'submit_field'  => '<div class="form-group submit-button">%1$s %2$s</div>',
-	'comment_field' => '<div class="form-group"><textarea id="comment" name="comment" required placeholder="' . esc_attr__( 'Comment *', 'homlisti' )
+	'comment_field' => '<div class="form-group"><textarea id="comment" name="comment" required placeholder="' . esc_attr__( 'Comment *', 'cl-classified' )
 	                   . '" class="form-control textarea" rows="10" cols="40"></textarea></div>',
 	'fields'        => apply_filters( 'comment_form_default_fields', $comment_form_fields ),
 ];
@@ -74,8 +78,11 @@ $comment_form_args = [
 
 <?php if ( comments_open() ): ?>
     <div class="content-block-gap"></div>
-    <div class="block-content blog-form">
-        <div class="widget-box">
+    <div class="site-content-block comment-reply-block">
+        <div class="main-title-block">
+            <h3 class="main-title"><?php esc_html_e( 'Leave a Comment', 'cl-classified' ); ?></h3>
+        </div>
+        <div class="main-content">
 			<?php comment_form( $comment_form_args ); ?>
         </div>
     </div>
--- template-parts/comments-callback.php
+++ template-parts/comments-callback.php
@@ -12,15 +12,15 @@ $human_time = human_time_diff( get_comment_time( 'U' ), current_time( 'timestamp
 <?php $tag = ( 'div' === $args['style'] ) ? 'div' : 'li'; ?>
 <<?php echo esc_html( $tag ); ?> id="comment-<?php comment_ID(); ?>" <?php comment_class( $args['has_children'] ? 'parent main-comments' : 'main-comments', $comment ); ?>>
 
-<div id="respond-<?php comment_ID(); ?>" class="each-comment media">
+<div id="respond-<?php comment_ID(); ?>" class="each-comment d-flex">
 
 	<?php if ( get_option( 'show_avatars' ) ): ?>
-        <div class="pull-left imgholder">
+        <div class="flex-shrink-0 imgholder">
 			<?php if ( 0 != $args['avatar_size'] ) echo get_avatar( $comment, $args['avatar_size'], "", false, array( 'class'=>'media-object' ) ); ?>
         </div>
 	<?php endif; ?>
 
-    <div class="media-body comments-body">
+    <div class="flex-grow-1 comments-body">
         <div class="comment-meta clearfix">
             <div class="comment-meta-left">
                 <h3 class="comment-author"><?php echo get_comment_author_link( $comment );?></h3>
