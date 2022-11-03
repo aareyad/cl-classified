d0b6051a35b7a8ed2682aa70362957dc3de0951c
aareyad <aliakbar.reyad@gmail.com>
2022-11-03 15:09:59 +0600

    fixed ads page responsive issue

--- inc/Listing_Functions.php
+++ inc/Listing_Functions.php
@@ -8,6 +8,7 @@
 namespace RadiusTheme\ClassifiedLite;
 
 use Rtcl\Controllers\Hooks\TemplateHooks;
+use Rtcl\Helpers\Functions;
 
 class Listing_Functions {
 
@@ -16,6 +17,7 @@ class Listing_Functions {
 	public function __construct() {
 		add_action( 'after_setup_theme', [ $this, 'theme_support' ] );
 		add_action( 'init', [ $this, 'rtcl_action_hook' ] );
+		add_action( 'init', [ $this, 'rtcl_filter_hook' ] );
 	}
 
 	public static function instance() {
@@ -31,9 +33,41 @@ class Listing_Functions {
 	}
 
 	public function rtcl_action_hook() {
+		if ( isset( $_GET['view'] ) && in_array( $_GET['view'], [ 'grid', 'list' ], true ) ) {
+			$view = esc_attr( $_GET['view'] );
+		} else {
+			$view = Functions::get_option_item( 'rtcl_general_settings', 'default_view', 'list' );
+		}
+		// remove action
 		remove_action( 'rtcl_before_main_content', [ TemplateHooks::class, 'breadcrumb' ], 6 );
-		if ( isset( $_GET['view'] ) && 'grid' === $_GET['view'] ) {
-			remove_action( 'rtcl_listing_loop_item', [ TemplateHooks::class, 'loop_item_excerpt' ], 70 );
+		remove_action( 'rtcl_listing_loop_item', [ TemplateHooks::class, 'loop_item_excerpt' ], 70 );
+		// add action
+		if ( 'list' === $view ) {
+			remove_action( 'rtcl_listing_loop_item', [ TemplateHooks::class, 'listing_price' ], 80 );
+			add_action( 'rtcl_listing_loop_item', [ __CLASS__, 'loop_item_category_price' ], 15 );
+			add_action( 'rtcl_listing_loop_item', [ TemplateHooks::class, 'loop_item_excerpt' ], 45 );
+		}
+	}
+
+	public function rtcl_filter_hook() {
+		add_filter( 'rtcl_listing_the_excerpt', function ( $excerpt ) {
+			return wp_trim_words( $excerpt, 25 );
+		} );
+	}
+
+	public static function loop_item_category_price() {
+		global $listing;
+		if ( $listing->has_category() && $listing->can_show_category() ) {
+			$category = $listing->get_categories();
+			$category = end( $category );
+			?>
+            <div class="listing-cat-price">
+                <a class="listing-categories" href="<?php echo esc_url( get_term_link( $category ) ); ?>">
+					<?php echo esc_html( $category->name ); ?>
+                </a>
+				<?php Functions::get_template( 'listing/loop/price' ); ?>
+            </div>
+			<?php
 		}
 	}
 
