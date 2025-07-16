4b3f43a0e998c23a4fac01c7f44735e8afe710c7
aareyad <aliakbar.reyad@gmail.com>
2025-07-16 18:13:56 +0600

    updated version

--- functions.php
+++ functions.php
@@ -10,11 +10,16 @@ if ( ! isset( $content_width ) ) {
 }
 
 add_action( 'init', 'cl_classified_load_textdomain', 20 );
+add_action( 'after_setup_theme', 'cl_classified_theme_load' );
 function cl_classified_load_textdomain() {
 	load_theme_textdomain( 'cl-classified', get_template_directory() . '/languages' );
 }
 
-define( 'CL_CLASSIFIED_VERSION', '1.7.0' );
+function cl_classified_theme_load() {
+	do_action( 'cl_classified_theme_init' );
+}
+
+define( 'CL_CLASSIFIED_VERSION', '1.7.1' );
 
 if ( ! defined( 'RT_DEBUG' ) ) {
 	require_once 'lib/updater/theme-updater.php';
@@ -22,5 +27,3 @@ if ( ! defined( 'RT_DEBUG' ) ) {
 require_once 'lib/class-tgm-plugin-activation.php';
 require_once 'inc/init.php';
 require_once 'inc/Customizer/sanitization.php';
-
-do_action( 'cl_classified_theme_init' );
